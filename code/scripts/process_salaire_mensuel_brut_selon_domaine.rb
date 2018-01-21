cb11920a049b85c7fccd8856c3428c2706e94bba
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2018-01-21 19:40:55 +0100

    selon_domaine no more used

--- code/scripts/process_salaire_mensuel_brut_selon_domaine.rb
+++
@@ -1,7 +0,0 @@
-require './processCSV.rb'
-
-processCSV = ProcessCSV.new('salaire_mensuel_brut_selon_domaine.csv', 'windows-1252')
-processCSV.addRegex("Homme", /\d+/)
-processCSV.addRegex("Femme", /\d+/)
-processCSV.addColumnToRemove("Niveau de qualification")
-processCSV.generateFile
